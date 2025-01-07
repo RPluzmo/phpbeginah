f14b88bf2378d2769150e07547860364b21f0bb8
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-07 11:09:39 +0200

    categories

--- blog/categories.php
+++ blog/categories.php
@@ -1,32 +1,20 @@
 <?php 
 
-require "functions.php";
 require "Database.php";
 $config=require("config.php");
 
-echo"woaw<br>";
-
 $db= new Database($config["database"]);//objects no database klases
 
-//Dump n Die
-//dd($posts);
-
-
-$sql = "SELECT * FROM posts";
+$sql = "SELECT * FROM categories";
 $params=[];
 if (isset($_GET["search_query"]) && $_GET["search_query"] !=""){
-    //mekleshanas logika
-    //dd("SELECT * FROM posts WHERE content LIKE '%" . $_GET["search_query"] . "%';");// parbaudam vai ir sql.. '% x %'
     $search_query="%". $_GET["search_query"]. "%";
-    $sql .=" WHERE content LIKE :search_query;";
+    $sql .=" WHERE category_name LIKE :search_query;";
     $params = ["search_query"=>$search_query];
 }
 $posts= $db->query($sql, $params)->fetchAll();
 //dd($sql);
 
-
-//method POST maina datubazee saturu
-//method GET ir default
 echo "<form >";
 echo "<input name='search_query' />";
 echo "<button>Meklēt</button>";
@@ -35,6 +23,6 @@ echo "</form>";
 
 echo"<ul>";
 foreach($posts as $post){
-    echo "<li>Id: ".$post['id']."<br>Content: ".$post['content']."<br><br>"
+    echo "<li>Kategorija: ".$post['category_name']."<br><br>"
 ;}
 echo"</ul>";
