aac900b0b6beb7ed607a2a0c88caf883ed329d46
RPluzmo <ipa23.r.pluzmo@vtdt.edu.lv>
2025-01-22 22:07:54 +0200

    validacijas klase

--- blog/Validator.php
+++ blog/Validator.php
@@ -1,16 +1,16 @@
 <?php
 class Validator{
-    public function string($value, $min = 1, $max = INF){
+    public static function string($value, $min = 1, $max = INF){
         $value = trim($value);
 
         return is_string($value)
                 && strlen($value) >= $min 
                 && strlen($value) <= $max;
     }
-    public function number($value, $min = 0, $max = INF){
+    public static function number($value, $min = 0, $max = INF){
         $value = trim($value);
 
-        return is_nimeric($value)
+        return is_numeric($value)
                 && $value >= $min
                 && $value <= $max;
     }
